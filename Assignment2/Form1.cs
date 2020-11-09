37c72759fcb815e1b695f00044cb81205c9f9ba3
Lewis Caldwell <RC0042005@student.riversidecollege.ac.uk>
2020-11-09 15:47:59 +0000

    Add project files.

---
+++ Assignment2/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                double length = double.Parse(textBox1.Text);
+                double width = double.Parse(textBox2.Text);
+                double height = double.Parse(textBox3.Text);
+
+                double volume = length * width * height;
+                double surfacearea =  2.0 * (length * height + length*width + width * height);
+                label1.Text = "volume =" + volume;
+                label2.Text = "surfacearea" + surfacearea;
+                
+
+
+
+
+
+
+
+
+
+
+
+            }
+            catch
+            {
+                MessageBox.Show("error on input");
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
